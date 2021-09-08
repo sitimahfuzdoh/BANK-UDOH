8df6474dd3a1ecec5d2d7cf9445693a2b57558b3
Siti Mahfuzdoh <mahfudzudho98@gmail.com>
2021-09-08 09:24:32 +0700

    first

---
+++ src/main/java/mii/co/id/BankAssignment/BankAssignmentApplication.java
@@ -0,0 +1,13 @@
+package mii.co.id.BankAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mii/co/id/BankAssignment/BankAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package mii.co.id.BankAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
